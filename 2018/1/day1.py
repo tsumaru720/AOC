9225fa78253a4ff9353cb8594ab33d9ce4256c5a
Simon Mott <simon@simonmott.co.uk>
2018-12-01 18:50:00 +0000

    Fuck python.

--- 2018/1/day1.py
+++ 2018/1/day1.py
@@ -6,32 +6,45 @@ if len(sys.argv) < 2:
 	print "Please choose from part [1] or part [2]"
 	exit()
 
-def part1():
-	frequency = 0
+
+duplicate = False
+frequency = 0
+history = {}
+
+def iterate():
+	global frequency, history, duplicate
 
 	file = open("input.txt")
+#	file = open("test.txt")
 	for line in file:
 		line = line.replace("\n","")
 		if line == "":
 			continue
 
-		if line[0] == "+":
-			frequency = frequency + int(line.replace("+",""))
-		elif line[0] == "-":
-			frequency = frequency - int(line.replace("-",""))
-		else:
-			exit("ERROR: Check input text")
+		frequency += int(line)
+
+		if frequency in history:
+			#We've seen this before!
+			duplicate = True
+			return frequency
+
+		history[frequency] = True
 	return frequency
 
+def part1():
+	return iterate()
 
 def part2():
-	print "Part 2 - Work in progress"
-
+	global duplicate, history
+	while True:
+		result = iterate()
+		if duplicate == True:
+			return result
 
 if sys.argv[1] == "1":
 	print part1()
 elif sys.argv[1] == "2":
-	part2()
+	print part2()
 else:
 	print "What?"
 	exit()
