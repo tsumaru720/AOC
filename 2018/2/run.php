09569e15a1cfe35340388163639993a7d9a2aad6
Simon Mott <simon@simonmott.co.uk>
2018-12-02 11:51:25 +0000

    day 2 - part 2

--- 2018/2/run.php
+++ 2018/2/run.php
@@ -52,7 +52,66 @@ function part1() {
 }
 
 function part2() {
-	echo "Placeholder";
+//	$file = fopen("part2_test.txt", "r");
+	$file = fopen("input.txt", "r");
+
+	while ($line = fgets($file)) {
+                $line = str_replace("\n","",$line);
+                $array = str_split($line);
+
+		$table[] = $line;
+	}
+
+	foreach ($table as $entry) {
+
+		$of_interest = array();
+
+		$halfway = (strlen($entry)/2);
+		$search[0] = substr($entry,0,$halfway);
+		$search[1] = substr($entry,$halfway);
+
+		foreach ($table as $key => $target) {
+			$lmatch = 0;
+			$rmatch = 0;
+			if ($target == $entry) { continue; }
+
+			$test[0] = substr($target,0,$halfway);
+			$test[1] = substr($target,$halfway);
+
+			if ($test[0] == $search[0]) {
+				$lmatch++;
+			}
+			if ($test[1] == $search[1]) {
+				$rmatch++;
+			}
+
+			if ((($lmatch == 1) && ($rmatch == 0)) || (($lmatch == 0) && ($rmatch == 1))) {
+				$of_interest[] = $key;
+			}
+		}
+
+		if (count($of_interest) == 1) {
+			foreach ($of_interest as $key) {
+				$difference = 0;
+				$one = str_split($entry);
+				$two = str_split($table[$key]);
+				foreach ($one as $i => $letter) {
+					if ($letter != $two[$i]) {
+						$difference++;
+						$errant_pos = $i;
+					}
+				}
+				if ($difference == 1) {
+					$one[$errant_pos] = "";
+					$result = join($one);
+					break;
+				}
+			}
+		}
+	}
+
+	fclose($file);
+	return $result;
 }
 
 
