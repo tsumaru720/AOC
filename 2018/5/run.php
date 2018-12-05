840b2c82ec9864be35bdaaf972f29b9229904bc2
Simon Mott <simon.mott@melbourne.co.uk>
2018-12-05 19:06:46 +0000

    part 2

--- 2018/5/run.php
+++ 2018/5/run.php
@@ -1,22 +1,47 @@
 #!/usr/bin/php
 <?php
 
-$pairs = array('aA' => '','Aa' => '','bB' => '','Bb' => '','cC' => '','Cc' => '','dD' => '','Dd' => '',
-		'eE' => '','Ee' => '','fF' => '','Ff' => '','gG' => '','Gg' => '','hH' => '','Hh' => '',
-		'iI' => '','Ii' => '','jJ' => '','Jj' => '','kK' => '','Kk' => '','lL' => '','Ll' => '',
-		'mM' => '','Mm' => '','nN' => '','Nn' => '','oO' => '','Oo' => '','pP' => '','Pp' => '',
-		'qQ' => '','Qq' => '','rR' => '','Rr' => '','sS' => '','Ss' => '','tT' => '','Tt' => '',
-		'uU' => '','Uu' => '','vV' => '','Vv' => '','wW' => '','Ww' => '','xX' => '','Xx' => '',
-		'yY' => '','Yy' => '','zZ' => '','Zz' => '');
+$alphabet = [
+	['a','A'], ['b','B'], ['c','C'], ['d','D'], ['e','E'], ['f','F'], ['g','G'], ['h','H'], ['i','I'],
+	['j','J'], ['k','K'], ['l','L'], ['m','M'], ['n','N'], ['o','O'], ['p','P'], ['q','Q'], ['r','R'],
+	['s','S'], ['t','T'], ['u','U'], ['v','V'], ['w','W'], ['x','X'], ['y','Y'], ['z','Z']
+	];
+
+$pairs = ['aA' => '','Aa' => '','bB' => '','Bb' => '','cC' => '','Cc' => '','dD' => '','Dd' => '','eE' => '',
+		'Ee' => '','fF' => '','Ff' => '','gG' => '','Gg' => '','hH' => '','Hh' => '','iI' => '','Ii' => '',
+		'jJ' => '','Jj' => '','kK' => '','Kk' => '','lL' => '','Ll' => '','mM' => '','Mm' => '','nN' => '',
+		'Nn' => '','oO' => '','Oo' => '','pP' => '','Pp' => '','qQ' => '','Qq' => '','rR' => '','Rr' => '',
+		'sS' => '','Ss' => '','tT' => '','Tt' => '','uU' => '','Uu' => '','vV' => '','Vv' => '','wW' => '',
+		'Ww' => '','xX' => '','Xx' => '','yY' => '','Yy' => '','zZ' => '','Zz' => ''];
 
 
 $polymer = file("input.txt",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)[0];
-$tmp = strtr($polymer,$pairs);
+echo strlen(react($polymer, $pairs));
+echo PHP_EOL;
 
-while ($tmp !== $polymer) {
-	$polymer = $tmp;
-	$tmp = strtr($polymer,$pairs);
+$result = array();
+
+
+foreach ($alphabet as $pair) {
+	$tmp = $polymer;
+	$tmp = str_replace($pair, '', $tmp);
+
+	$x = react($tmp, $pairs);
+
+	$result[] = strlen($x);;
 }
 
-echo strlen($polymer);
+echo min($result);
 echo PHP_EOL;
+
+function react($polymer, $pairs) {
+
+	$tmp = strtr($polymer,$pairs);
+
+	while ($tmp !== $polymer) {
+		$polymer = $tmp;
+		$tmp = strtr($polymer,$pairs);
+	}
+	
+	return $tmp;
+}
