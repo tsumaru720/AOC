bc780ab90005f45d316d23c32bbbfdfabf7fe1e4
Simon Mott <simon.mott@melbourne.co.uk>
2018-12-05 20:28:56 +0000

    redo day 1 in php - part 2

--- 2018/1/run.php
+++ 2018/1/run.php
@@ -2,15 +2,32 @@
 <?php
 
 $freq = 0;
+$history = [];
+$dupe = 0;
+
 $changes = file('input.txt',FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 iterate($freq, $changes);
 echo $freq;
 echo PHP_EOL;
 
+while ($dupe === 0) {
+	iterate($freq, $changes);
+}
+echo $dupe;
+echo PHP_EOL;
+
 function iterate(&$freq, &$changes) {
+	global $history, $dupe;
 	$s = count($changes);
 	for ($k = 0; $k < $s; $k++) {
 		$freq += (int) $changes[$k];
+
+		if (array_key_exists($freq, $history)) {
+			$dupe = $freq;
+			break;
+		}
+
+		$history[$freq] = true;
 	}
 }
